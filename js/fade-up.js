7580676b73c214da17360956be3ce1e90dfb87bc
Kelvin Melo <kmelo7882@gmail.com>
2025-11-03 17:15:18 -0300

    funções de performance adicionadas

--- js/fade-up.js
+++ js/fade-up.js
@@ -1,15 +1,22 @@
 const fadeUpElementos = document.querySelectorAll('.fade-up');
+let isScrolling = false;
 
 function checkFadeUpElements(){
-    fadeUpElementos.forEach((element) => {
-        const elementTop = element.getBoundingClientRect().top;
+    if(!isScrolling){
+        window.requestAnimationFrame(() =>{
+            fadeUpElementos.forEach((element) => {
+            const elementTop = element.getBoundingClientRect().top;
 
-        const windowHeight = window.innerHeight;
+            const windowHeight = window.innerHeight;
 
-        if(elementTop < windowHeight * 0.8){
-            element.classList.add('show');
-        }
-    })
+            if(elementTop < windowHeight * 0.8){
+                element.classList.add('show');
+            }
+        });
+        isScrolling = false;
+        })
+    }
+    isScrolling = true;
 }
 
 window.addEventListener('scroll', checkFadeUpElements)
