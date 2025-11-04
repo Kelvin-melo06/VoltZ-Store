276b8e066e9a1db6ddf40efb6a50e06dd9691f10
Kelvin Melo <kmelo7882@gmail.com>
2025-11-04 14:46:40 -0300

    alterações no mobile, bug consertado no desktop em relação a nav

--- js/Hamburguer.js
+++ js/Hamburguer.js
@@ -1,8 +1,23 @@
 const nav = document.querySelector('nav');
-const hamburguerIcon = document.querySelector('.hamburguer-icon');
+const hamburguer = document.querySelector('.hamburguer-icon i');
+
 
 function toggleMenu(){
+   const willOpen = !nav.classList.contains('open');
+
    nav.classList.toggle('open');
+
+   hamburguer.classList.toggle('hamburguer-anim');
+
+   requestAnimationFrame(() =>{
+      setTimeout(() =>{
+         if(willOpen){
+            hamburguer.classList.replace('fa-bars', 'fa-xmark');
+         }else{
+            hamburguer.classList.replace('fa-xmark', 'fa-bars');
+         }
+      }, 180);
+   });
 }
 
-hamburguerIcon.addEventListener('click', toggleMenu)
+hamburguer.addEventListener('click', toggleMenu)
