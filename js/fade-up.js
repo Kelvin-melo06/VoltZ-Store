868a2015863f587494f71071e15e5a738e95d240
Kelvin Melo <kmelo7882@gmail.com>
2025-11-11 20:21:59 -0300

    revisões ks

--- js/fade-up.js
+++ js/fade-up.js
@@ -4,25 +4,20 @@ const fadeUpElementos = document.querySelectorAll('.fade-up');
 let isScrolling = false;
 
 function checkFadeUpElements(){
-    // Throttling: Se já estiver agendado, não faz nada
-    if (!isScrolling) {
-        
-        window.requestAnimationFrame(() => { // Sincroniza com o frame de renderização
-            
-            fadeUpElementos.forEach((element) => {
-                const elementTop = element.getBoundingClientRect().top;
+    if(!isScrolling){
+        window.requestAnimationFrame(() => {
+            fadeUpElementos.forEach((elements) => {
+                const elementsTop = elements.getBoundingClientRect().top;
                 const windowHeight = window.innerHeight;
 
-                // Gatilho em 80% da tela
-                if (elementTop < windowHeight * 0.8) {
-                    element.classList.add('show');
+                if(elementsTop < windowHeight * 0.8){
+                    elements.classList.add('show');
                 }
-            });
-
-            isScrolling = false; // Libera o Throttling para o próximo frame
-        });
+            })
+            isScrolling = false;
+        })
     }
-    isScrolling = true; // Seta a flag para 'ocupado'
+    isScrolling = true;
 }
 
 window.addEventListener('scroll', checkFadeUpElements);
