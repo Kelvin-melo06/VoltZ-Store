2faf1a03c33f9dca77287c66831a55d4db533b20
Kelvin Melo <kmelo7882@gmail.com>
2026-03-19 16:54:13 -0300

    fix(docs main): arrumando doc errada do main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -21,40 +21,40 @@ import App from './App.jsx';
  * que é onde todo o conteúdo do React será renderizado.
  */
 createRoot(document.getElementById('root')).render(
-
-  /**
-   * StrictMode:
-   * -----------
-   * Ferramenta do React que ajuda a identificar possíveis problemas no código,
-   * tornando o ambiente de desenvolvimento mais "rigoroso" com erros e boas práticas.
-   */
   <StrictMode>
+    {/* 
+      StrictMode:
+      -----------
+      Ferramenta do React que ajuda a identificar possíveis problemas no código,
+      tornando o ambiente de desenvolvimento mais "rigoroso" com erros e boas práticas.
+    */}
 
-    /**
-     * AppProviders:
-     * -------------
-     * Componente responsável por agrupar todos os Providers da aplicação.
-     *
-     * Atualmente ele contém:
-     * - CartContext (carrinho)
-     * - ModalContext (controle do modal)
-     *
-     * Ele utiliza "children" para envolver o App, permitindo que toda a aplicação
-     * tenha acesso aos estados globais fornecidos pelos contextos.
-     */
     <AppProviders>
+      {/* 
+        AppProviders:
+        -------------
+        Componente responsável por agrupar todos os Providers da aplicação.
 
-      /**
-       * App:
-       * ----
-       * Componente principal da aplicação.
-       * Tudo que aparece na tela está dentro dele.
-       *
-       * Como ele está dentro do AppProviders,
-       * ele consegue acessar todos os contextos globais (carrinho e modal).
-       */
+        Atualmente ele contém:
+        - CartContext (carrinho)
+        - ModalContext (controle do modal)
+
+        Ele utiliza "children" para envolver o App, permitindo que toda a aplicação
+        tenha acesso aos estados globais fornecidos pelos contextos.
+      */}
+
       <App />
 
+      {/* 
+        App:
+        ----
+        Componente principal da aplicação.
+        Tudo que aparece na tela está dentro dele.
+
+        Como ele está dentro do AppProviders,
+        ele consegue acessar todos os contextos globais (carrinho e modal).
+      */}
+      
     </AppProviders>
   </StrictMode>
 );
