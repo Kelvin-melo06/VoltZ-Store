e5d912345bf3ba2ed0ecd7e05d4d9adfda17814f
Kelvin Melo <kmelo7882@gmail.com>
2026-03-19 17:54:38 -0300

    feat(doc SectionTitle): Documentação SectionTitle Feita

--- src/Components/ui/SectionTitle.jsx
+++ src/Components/ui/SectionTitle.jsx
@@ -1,36 +1,55 @@
+/**
+ * SectionTitle.jsx
+ * ----------------------------------------
+ * Componente reutilizável para títulos das seções da aplicação.
+ *
+ * Sua principal função é padronizar o estilo dos títulos,
+ * evitando repetição de código e facilitando a manutenção.
+ */
 
-
-const SectionTitle = ({children}) => {
+const SectionTitle = ({ children }) => {
   return (
-    <div 
-    className="
-    font-['Orbitron']
-    text-[28px]
-    md:text-4xl
-    mb-12
-    text-center
-    text-[#F2F2F2]
-    relative
-    inline-block
-    after:content-['']
-    after:absolute
-    after:left-1/2
-    after:bottom-[-10px]
-    after:h-[3px]
-    after:w-[0px]
-    after:-translate-x-1/2
-    after:bg-gradient-to-r
-    after:from-transparent
-    after:via-[#00BFFF]
-    after:to-transparent
-    after:transition-all
-    after:duration-300
-    hover:after:w-[120px]
-    fade-up
-    ">
-        {children}
-    </div>
-  )
-}
 
-export default SectionTitle
+    /**
+     * children:
+     * ---------
+     * Representa o conteúdo que será passado para o componente.
+     *
+     * Exemplo de uso:
+     * <SectionTitle>Produtos em destaque</SectionTitle>
+     *
+     * O texto passado será renderizado aqui e receberá toda a estilização.
+     */
+    <div
+      className="
+        font-['Orbitron']
+        text-[28px]
+        md:text-4xl
+        mb-12
+        text-center
+        text-[#F2F2F2]
+        relative
+        inline-block
+        after:content-['']
+        after:absolute
+        after:left-1/2
+        after:bottom-[-10px]
+        after:h-[3px]
+        after:w-[0px]
+        after:-translate-x-1/2
+        after:bg-gradient-to-r
+        after:from-transparent
+        after:via-[#00BFFF]
+        after:to-transparent
+        after:transition-all
+        after:duration-300
+        hover:after:w-[120px]
+        fade-up
+      "
+    >
+      {children}
+    </div>
+  );
+};
+
+export default SectionTitle;
